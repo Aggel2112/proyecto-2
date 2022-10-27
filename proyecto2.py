1d4bd0b734b57420cc27dc3b85398ef5066aa11f
aggel2112 <angelrene021@gmail.com>
2022-10-27 15:23:36 -0600

    update

---
+++ proyecto2.py
@@ -0,0 +1,38 @@
+from cProfile import label
+from cgitb import text
+from pyclbr import Function
+from tkinter import *
+from wsgiref.validate import validator
+# definimos los comandos que aran que aumente y disminuya de uno en uno el contador 
+
+def incrementar():
+    valor = int(label_valor["text"])
+    label_valor["text"] = f"{valor + 1} "
+
+def disminuir():
+    valor = int(label_valor["text"])
+    label_valor["text"] = f"{valor - 1}" 
+
+ventana = Tk()
+ventana.title("CONTADOR")
+ventana.geometry("250x150")
+ventana.resizable(0,0)
+ventana.rowconfigure(0, minsize=50, weight=1)
+ventana.columnconfigure([0, 1, 2], minsize=50, weight=1)
+
+boton_decre = Button(ventana, text="DISMINUIR", activebackground="red", command= disminuir)
+boton_decre.grid(row=0, column=0, sticky="nsew")
+
+boton_incre = Button(ventana, text="AUMENTAR",activebackground="green", command= incrementar)
+boton_incre.grid(row=0, column=2, sticky="nsew")
+
+label_valor = Label(ventana, text=0, fg="black", font=("arial", 35))
+label_valor.grid(row=0, column=1)
+
+if  label_valor["text"] > int (0):
+    label_valor.config(background="green")
+
+elif label_valor["text"] < int (0)  :
+    label_valor.config(background="white")
+
+ventana.mainloop()
